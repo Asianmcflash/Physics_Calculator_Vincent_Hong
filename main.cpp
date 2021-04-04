eb78493a63d31bfb6d84ed1360f1700f3970f73d
Vincent Hong <>
2021-04-04 04:45:31 +0000

    Finished

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 void showMenu()
@@ -19,31 +20,31 @@ void showMenu()
 void velocity(double ds, double dt, string dtUnits, string dsUnits)
 {
   double v = ds / dt;
-  cout << v << " " << dsUnits << "/" << dtUnits << endl;
+  cout << ds << dsUnits << " " << " / " << dt << dtUnits << " = " << setprecision(5) << v << " " << dsUnits << "/" << dtUnits << endl;
 }
 
 void acceleration(double dv, double dt, string dtUnits, string dvUnits)
 {
   double a = dv / dt;
-  cout << a << " " << dvUnits << "/" << dtUnits << endl;
+  cout << dv << dvUnits << " / " << dt << dtUnits << " = " << setprecision(5) << a << " " << dvUnits << "/" << dtUnits << endl;
 }
 
 void newton(double m, double a, string units, string unitsTwo)
 {
   double f = m * a;
-  cout << f << " " << units << "/" << unitsTwo << endl;
+  cout << m << units << " * " << a << unitsTwo << " = " << setprecision(5) << f << " " << units << "/" << unitsTwo << endl;
 }
 
 void weight(double m, string mw)
 {
   double w = m * 9.8;
-  cout << w << " " << mw << endl;
+  cout << m << mw << " * g(9.8 for gravity) = " << setprecision(5) << w << mw << endl;
 }
 
 void momentum(double m, double v, string mm, string mv)
 {
   double p = m * v;
-  cout << p << " " << mm << " " << mv << "/s" << endl;
+  cout << m << mm << " * " << v << mv << " = " << setprecision(5) << p << " " << mm << " " << mv << "/s" << endl;
 }
 
 void miniDetermine(int number)
@@ -57,37 +58,38 @@ void miniDetermine(int number)
   double vSquared;
   string units;
   double v_bar;
+  string unitsTwo;
   if(number == 1)
   {
-    cout << "Enter v0, a, t, and units: ";
-    cin >> vZero >> a >> t >> units;
-    cout << vZero << " " << a << " " << t << endl;
+    cout << "Enter Velocity(at 0), Acceleration, Time, Measurement for Velocity, and Measurement of Time: ";
+    cin >> vZero >> a >> t >> units >> unitsTwo;
+    cout << "Entered: " << vZero << " " << a << " " << t << " " << unitsTwo << endl;
     v = vZero + (a * t);
-    cout << v << " " << units << endl;
+    cout << vZero << units << " + (" << a << " * " << t << ") = " << setprecision(5) << v << " " << units << "/" << unitsTwo << endl;
   }
   else if(number == 2)
   {
     cout << "Enter s0, v0, t, a, and units: ";
     cin >> sZero >> vZero >> t >> a >> units;
-    cout << sZero << " " << vZero << " " << t << " " << a << " " << units << endl;
+    cout << "Entered: " << sZero << " " << vZero << " " << t << " " << a << " " << units << endl;
     s = sZero + (vZero * t) + (0.5 * a * t * t);
-    cout << s << " " << units << endl;
+    cout << setprecision(5) << s << " " << units << endl;
   }
   else if(number == 3)
   {
     cout << "Enter v0, a, s, s0, and units: ";
     cin >> vZero >> a >> s >> sZero >> units;
-    cout << vZero << " " << a << " " << s << " " << sZero << " " << units << endl;
+    cout << "Entered: " << vZero << " " << a << " " << s << " " << sZero << " " << units << endl;
     vSquared = (vZero * vZero) + (2 * (a * (s - sZero)));
-    cout << vSquared << " " << units << endl;
+    cout << setprecision(5) << vSquared << " " << units << endl;
   }
   else
   {
     cout << "Enter v, vZero, and units: ";
     cin >> v >> vZero >> units;
-    cout << v << " " << vZero << " " << units << endl;
+    cout << "Entered: " << v << " " << vZero << " " << units << endl;
     v_bar = 0.5 * (v + vZero);
-    cout << v_bar << " " << units << endl;
+    cout << setprecision(5) << v_bar << " " << units << endl;
   }
 }
 
@@ -114,7 +116,7 @@ void determine(string letter)
     cout << "A: Velocity Calculator" << endl;
     cout << "Enter Speed Units(Kg, Mph, etc), Speed(Number), Time measurement(Hr, Min, etc), and Time(How Long), respectively: ";
     cin >> dsUnits >> ds >> dtUnits >> dt;
-    cout << dsUnits << " " << ds << " " << dtUnits << " " << dt << endl;
+    cout << "Entered: " << dsUnits << " " << ds << " " << dtUnits << " " << dt << endl;
     velocity(ds, dt, dtUnits, dsUnits);
   }
   else if(letter == "B" || letter == "b")
@@ -122,7 +124,7 @@ void determine(string letter)
     cout << "B: Acceleration Calculator" << endl;
     cout << "Enter Speed Units(Kg, Mph, etc), Speed(Number), Time measurement(Hr, Min, etc), and Time(How Long), respectively: ";
     cin >> dvUnits >> dv >> dtUnits >> dt;
-    cout << dvUnits << " " << dv << " " << dtUnits << " " << dt << endl;
+    cout << "Entered: " << dvUnits << " " << dv << " " << dtUnits << " " << dt << endl;
     acceleration(dv, dt, dtUnits, dvUnits);
   }
   else if(letter == "C" || letter == "c")
@@ -138,7 +140,7 @@ void determine(string letter)
     cout << "D: Newton's Second Law" << endl;
     cout << "Enter Mass, Acceleration, Mass Measurement, and Acceleration Measurement, respectively: ";
     cin >> m >> a >> units >> unitsTwo;
-    cout << m << " " << a << " " << units << unitsTwo << endl;
+    cout << "Entered: " << m << " " << a << " " << units << unitsTwo << endl;
     newton(m, a, units, unitsTwo);
   }
   else if(letter == "E" || letter == "e")
@@ -150,7 +152,7 @@ void determine(string letter)
     cout << "F: Weight(Earth)" << endl;
     cout << "Enter Weight, and Measurement of Weight, respectively";
     cin >> w >> mw;
-    cout << w << " " << mw << endl;
+    cout << "Entered: " << w << " " << mw << endl;
     weight(w, mw);
   }
   else if(letter == "G" || letter == "g")
@@ -158,7 +160,7 @@ void determine(string letter)
     cout << "G: Momentum" << endl;
     cout << "Enter Mass, Velocity, Measurement of Mass, and Measurement of Velocity, respectively";
     cin >> m >> v >> mm >> mv;
-    cout << m << " " << v << " " << mm << " " << mv << endl;
+    cout << "Entered: " << m << " " << v << " " << mm << " " << mv << endl;
     momentum(m, v, mm, mv);
   }
   else if(letter == "X" || letter == "x")
